c862c6ede837f02d6d9b954f2fdcb76b87f647ab
ITesserakt <potryas66@mail.ru>
2023-11-23 14:05:21 +0300

    test for login and logout

--- 8_selenium/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-# @pytest.fixture(autouse=True)
-# def driver():
-#     # Инициализация WebDriver
-#     driver = webdriver.Firefox()
-#     yield driver
-#     driver.quit()
--- 8_selenium/test.py
+++ 8_selenium/test.py
@@ -1,16 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+import time
 # driver = webdriver.Firefox()
 # driver.get("https://www.saucedemo.com")
 
-def test_login():
+def init_website():
     driver = webdriver.Firefox()
     driver.get("https://www.saucedemo.com")
+    return driver
+def test_login():
+    driver = init_website()
     element = driver.find_element(By.NAME, 'user-name')
     element.send_keys("standard_user")
+    time.sleep(0.6)
     element = driver.find_element(By.NAME, 'password')
     element.send_keys("secret_sauce")
+    time.sleep(0.6)
     driver.find_element(By.NAME, 'login-button').click()
     assert "https://www.saucedemo.com/inventory.html" in driver.current_url
+
+def test_logout():
+    driver = init_website()
+    element = driver.find_element(By.NAME, 'user-name')
+    element.send_keys("standard_user")
+    time.sleep(0.6)
+    element = driver.find_element(By.NAME, 'password')
+    element.send_keys("secret_sauce")
+    time.sleep(0.6)
+    driver.find_element(By.NAME, 'login-button').click()
+    time.sleep(0.6)
+    driver.find_element(By.ID, 'react-burger-menu-btn').click()
+    time.sleep(0.6)
+    driver.find_element(By.ID, 'logout_sidebar_link').click()
+    assert "https://www.saucedemo.com" in driver.current_url
