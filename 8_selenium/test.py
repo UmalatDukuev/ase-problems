a96a81d662fa756f8a54500d98ba55bc4bf47b73
ITesserakt <potryas66@mail.ru>
2023-11-24 14:14:24 +0300

    test for all sorts

--- 8_selenium/test.py
+++ 8_selenium/test.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-# driver = webdriver.Firefox()
-# driver.get("https://www.saucedemo.com")
+from selenium.webdriver.support.select import Select
 
 def init_website():
     driver = webdriver.Firefox()
     driver.get("https://www.saucedemo.com")
     return driver
+
 # def test_login():
 #     driver = init_website()
 #     element = driver.find_element(By.NAME, 'user-name')
@@ -40,6 +40,20 @@ def init_website():
 
 #     driver.find_element(By.ID, 'logout_sidebar_link').click()
 #     assert "https://www.saucedemo.com" in driver.current_url
+
+# @pytest.mark.parametrize("str1, str2, expected_result", [("", "", ""),
+#                                                          ("", " ", " "),
+#                                                          ("123", "456!", "123456!"),
+#                                                          ("abc", "def", "abcdef"),
+#                                                          ("123abc", "456def", "123abc456def"),
+#                                                          ("-123", "+456", "-123+456"),
+#                                                          ("!123@", "_456(", "!123@_456("),
+#                                                          ("-123", "+456", "-123+456"),
+#                                                          ("<p>123</p>", "456", "<p>123</p>456"),
+#                                                          ])
+# def test_success_concat(str1, str2, expected_result):
+#     assert concat(str1, str2) == expected_result
+
 
 # def test_shopping():
 #     driver = init_website()
@@ -97,22 +111,106 @@ def init_website():
 #     time.sleep(0.4)
 #     driver.find_element(By.NAME, 'login-button').click()
 #     time.sleep(0.4)
+#     items = driver.find_elements(By.CLASS_NAME, 'inventory_item_name')
+#     print()
+#     list = []
+#     for item in items:
+#         list.append(item.text)
+#     print(list)
+#     sorted_list = sorted(list)
+#     print(sorted_items)
+#     for i in range(len(list)):
+#         assert list[i] == sorted_list[i]
 
 
 # def test_sort_Z_to_A():
+#     driver = init_website()
+#     element = driver.find_element(By.NAME, 'user-name')
+#     element.send_keys("standard_user")
+#     time.sleep(0.4)
+#     element = driver.find_element(By.NAME, 'password')
+#     element.send_keys("secret_sauce")
+#     time.sleep(0.4)
+#     driver.find_element(By.NAME, 'login-button').click()
+#     time.sleep(0.4)
+#     select = driver.find_element(By.CLASS_NAME, 'product_sort_container')
+#     select.click()
+#     select_element = driver.find_element(By.CSS_SELECTOR, ".product_sort_container")
+#     select = Select(select_element)
+#     select.select_by_value("za")
+#     items = driver.find_elements(By.CLASS_NAME, 'inventory_item_name')
+#     print()
+#     list = []
+#     for item in items:
+#         list.append(item.text)
+#     print(list)
+#     sorted_list = sorted(list, reverse=True)
+#     print(sorted_list)
+#     for i in range(len(list)):
+#         assert list[i] == sorted_list[i]
+
 # def test_sort_low_to_high():
+#     driver = init_website()
+#     element = driver.find_element(By.NAME, 'user-name')
+#     element.send_keys("standard_user")
+#     time.sleep(0.4)
+#     element = driver.find_element(By.NAME, 'password')
+#     element.send_keys("secret_sauce")
+#     time.sleep(0.4)
+#     driver.find_element(By.NAME, 'login-button').click()
+#     time.sleep(0.4)
+#     select = driver.find_element(By.CLASS_NAME, 'product_sort_container')
+#     select.click()
+#     select_element = driver.find_element(By.CSS_SELECTOR, ".product_sort_container")
+#     select = Select(select_element)
+#     select.select_by_value("lohi")
+#     items = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+#     print()
+#     list = []
+#     for item in items:
+#         list.append(item.text)
+#     print(list)
+#     sorted_list =sorted(list, key=lambda x: float(x[1:]))
+#     print(sorted_list)
+#     for i in range(len(list)):
+#         assert list[i] == sorted_list[i]
+
 # def test_sort_high_to_low():
+#     driver = init_website()
+#     element = driver.find_element(By.NAME, 'user-name')
+#     element.send_keys("standard_user")
+#     time.sleep(0.4)
+#     element = driver.find_element(By.NAME, 'password')
+#     element.send_keys("secret_sauce")
+#     time.sleep(0.4)
+#     driver.find_element(By.NAME, 'login-button').click()
+#     time.sleep(0.4)
+#     select = driver.find_element(By.CLASS_NAME, 'product_sort_container')
+#     select.click()
+#     select_element = driver.find_element(By.CSS_SELECTOR, ".product_sort_container")
+#     select = Select(select_element)
+#     select.select_by_value("hilo")
+#     items = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+#     print()
+#     list = []
+#     for item in items:
+#         list.append(item.text)
+#     print(list)
+#     sorted_list =sorted(list, key=lambda x: float(x[1:]), reverse=True)
+#     print(sorted_list)
+#     for i in range(len(list)):
+#         assert list[i] == sorted_list[i]
 
-
-def test_locked_out():
-    driver = init_website()
-    element = driver.find_element(By.NAME, 'user-name')
-    element.send_keys("locked_out_user")
-    time.sleep(0.4)
-    element = driver.find_element(By.NAME, 'password')
-    element.send_keys("secret_sauce")
-    time.sleep(0.4)
-    driver.find_element(By.NAME, 'login-button').click()
-    time.sleep(0.4)
-    error_message = driver.find_element(By.CLASS_NAME, 'error-message-container')
-    assert error_message.is_displayed()
+#
+# def test_locked_out():
+#     driver = init_website()
+#     element = driver.find_element(By.NAME, 'user-name')
+#     element.send_keys("locked_out_user")
+#     time.sleep(0.4)
+#     element = driver.find_element(By.NAME, 'password')
+#     element.send_keys("secret_sauce")
+#     time.sleep(0.4)
+#     driver.find_element(By.NAME, 'login-button').click()
+#     time.sleep(0.4)
+#     error_message = driver.find_element(By.CLASS_NAME, 'error-message-container')
+#     assert error_message.is_displayed()
